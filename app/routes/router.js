056c9f3d954b2f81f59b7bae674b06ab16d876c3
Gabriel Soares <rm93907@estudante.fieb.edu.br>
2025-06-27 11:30:20 -0300

    commit inicial

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const app = express();
+const port = 3000;
 const router = express.Router();
 const {body, validationResult} = require("express-validator")
 
-router.get("/", (req, res) =>{
-    res.render("pages/index");
-}   );
+app.use(express.static("./app/public"));
 
-res.render("pages/index")
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
 
-module.exports = router;
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const rotaPrincipal = require("./app/routes/router");
+app.use("/", rotaPrincipal);
+
+app.listen(port, ()=>{
+    console.log(`Servidor onLine!\nhttp://localhost:${port}`);
+})
